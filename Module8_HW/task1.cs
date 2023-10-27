4045554fc59289e3a212c0399d6485b6af17f86d
samet <samet@DESKTOP-OEAKA56>
2023-10-27 22:48:57 +0600

    Add project files.

---
+++ Module8_HW/task1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class SquaredArray
+{
+    public int[] Array;
+
+    public SquaredArray(int size)
+    {
+        Array = new int[size];
+    }
+
+    public int this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= Array.Length)
+            {
+                throw new IndexOutOfRangeException("The index is outside the range of the array");
+            }
+            return Array[index];
+        }
+        set
+        {
+            if (index < 0 || index >= Array.Length)
+            {
+                throw new IndexOutOfRangeException("The index is outside the range of the array");
+            }
+            Array[index] = value * value; 
+        }
+    }
+}
+
+class App
+{
+    static void Main()
+    {
+        int size = 5;
+        SquaredArray squaredArray = new SquaredArray(size);
+
+        for (int i = 0; i < size; i++)
+        {
+            squaredArray[i] = i + 1;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine($"Element {i}: {squaredArray[i]}");
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Module8_HW/task2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        double heatingRate = 10; 
+        double waterRate = 5; 
+        double gasRate = 7; 
+        double repairRate = 3; 
+
+        double roomArea = 100; 
+        int numberOfResidents = 4;
+        string season = "Winter"; 
+        bool veteranOfLabor = true;
+        bool warVeteran = false; 
+
+        double heatingCost = roomArea * heatingRate;
+        double waterCost = numberOfResidents * waterRate;
+        double gasCost = numberOfResidents * gasRate;
+        double repairCost = roomArea * repairRate;
+
+        if (season == "Winter")
+        {
+            heatingCost *= 1.2; 
+        }
+
+        double totalCost = heatingCost + waterCost + gasCost + repairCost;
+
+        if (veteranOfLabor)
+        {
+            totalCost *= 0.7; 
+        }
+
+        if (warVeteran)
+        {
+            totalCost *= 0.5; 
+        }
+
+        Console.WriteLine("Type of payment\tAccrued\tThe total discount\tTotal");
+        Console.WriteLine($"Heating\t{heatingCost:C}\t-\t{heatingCost:C}");
+        Console.WriteLine($"Water\t\t{waterCost:C}\t-\t{waterCost:C}");
+        Console.WriteLine($"Gas\t\t{gasCost:C}\t-\t{gasCost:C}");
+        Console.WriteLine($"Repair\t\t{repairCost:C}\t-\t{repairCost:C}");
+        Console.WriteLine($"Total Cost\t\t{totalCost:C}");
+    }
+}
